435c45d46b30a423cc9545e00e6b66d1f9b2e735
architg-op <archit.g1998@gmail.com>
2024-03-31 22:56:05 +0530

    Added delete job functionality and the notification toasts

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,21 @@ import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
 
 const App = () => {
-  const addJob = (newJob) => {
-    console.log(newJob);
+  const addJob = async (newJob) => {
+    const res = await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  const deleteJob = async (jobid) => {
+    const res = await fetch(`/api/jobs/${jobid}`, {
+      method: "DELETE",
+    });
   };
 
   const router = createBrowserRouter(
@@ -22,8 +35,16 @@ const App = () => {
         <Route index element={<HomePage />} />)
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
-        <Route path="jobs/job/:id" element={<JobPage />} loader={jobLoader} />
-        <Route path="/job/:id" element={<JobPage />} loader={jobLoader} />
+        <Route
+          path="jobs/job/:id"
+          element={<JobPage deleteJobSubmit={deleteJob} />}
+          loader={jobLoader}
+        />
+        <Route
+          path="/job/:id"
+          element={<JobPage deleteJobSubmit={deleteJob} />}
+          loader={jobLoader}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,10 +1,26 @@
 import { useParams, useLoaderData } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
-const JobPage = () => {
+const JobPage = ({ deleteJobSubmit }) => {
   const { id } = useParams();
   const job = useLoaderData();
+  const navigate = useNavigate();
+
+  const deleteJob = (e) => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this job posting?"
+    );
+    if (!confirm) {
+      return;
+    }
+    deleteJobSubmit(id);
+    toast.success("Job deleted successfully!");
+    navigate("/jobs");
+  };
+
   return (
     <>
       <section>
@@ -81,7 +97,10 @@ const JobPage = () => {
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button
+                  onClick={deleteJob}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
                   Delete Job
                 </button>
               </div>
