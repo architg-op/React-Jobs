1d44f80d1a7bfcd7011d627cadc92789fd5ea261
architg-op <archit.g1998@gmail.com>
2024-05-15 19:01:22 +0530

    Deployed Live

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/React-Jobs/',
   server: {
     port: 3000,
     proxy: {
